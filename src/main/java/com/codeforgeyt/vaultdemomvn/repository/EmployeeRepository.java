62a4128baff7f0246e09ca97e97b555aa2443592
wspasindupanthaka <wpasindupanthaka@gmail.com>
2021-12-14 12:14:54 +0530

    Removing unused imports

--- src/main/java/com/codeforgeyt/vaultdemomvn/repository/EmployeeRepository.java
+++ src/main/java/com/codeforgeyt/vaultdemomvn/repository/EmployeeRepository.java
@@ -3,8 +3,6 @@ package com.codeforgeyt.vaultdemomvn.repository;
 import com.codeforgeyt.vaultdemomvn.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
 }
